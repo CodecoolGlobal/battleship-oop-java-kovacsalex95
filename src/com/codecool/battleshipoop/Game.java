322dd0d7a0d8116ee40864ddb4a1c1101f6f8457
kovacsalex <kovacsalex95@gmail.com>
2021-05-25 15:04:18 +0200

    new essential variables

--- src/com/codecool/battleshipoop/Game.java
+++ src/com/codecool/battleshipoop/Game.java
@@ -1,10 +1,25 @@
 package com.codecool.battleshipoop;
 
+import java.awt.geom.Point2D;
+
 public class Game {
     private MainWindow window;
+
+    private int boardSize = 10;
+
+    private int round = 0;
+    private int player = 0;
+
+    private Ship[] player1Ships;
+    private Ship[] player2Ships;
+
+    private Point2D[] player1Hits;
+    private Point2D[] player2Hits;
 
     public Game(MainWindow window)
     {
         this.window = window;
+
+        // player1Hits[0] = new Point2D.Double(0, 0);
     }
 }
