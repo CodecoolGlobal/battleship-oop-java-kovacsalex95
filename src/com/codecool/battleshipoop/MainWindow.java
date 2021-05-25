aeb0224b82351c6b7f3341fb499dba25020b7132
kovacsalex <kovacsalex95@gmail.com>
2021-05-25 12:27:11 +0200

    dunno

--- src/com/codecool/battleshipoop/MainWindow.java
+++ src/com/codecool/battleshipoop/MainWindow.java
@@ -4,20 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class MainWindow {
-    private JPanel panel_main;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JPanel panel_board_container;
-    private JPanel panel_board_1;
-    private JPanel panel_board_2;
-    private JToolBar toolbar_functions;
+    private JPanel panelMain;
+    private JButton newGameButton;
+    private JButton dontPressMeButton;
+    private JButton creditsButton;
+    private JButton exitGameButton;
+    private JPanel panelBoardContainer;
+    private JPanel panelBoard1;
+    private JPanel panelBoard2;
+    private JToolBar toolbarFunctions;
 
     public static void main(String args[])
     {
         JFrame frame = new JFrame("Battleship Hokkaido");
-        frame.setContentPane(new MainWindow().panel_main);
+        frame.setContentPane(new MainWindow().panelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(800, 500));
         frame.pack();
