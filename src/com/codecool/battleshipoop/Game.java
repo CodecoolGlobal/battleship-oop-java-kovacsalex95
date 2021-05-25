4c03dc6249de64ba721dc67ef6b99284df16c719
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-05-25 16:36:35 +0200

    Add board size and create getter to this

--- src/com/codecool/battleshipoop/Game.java
+++ src/com/codecool/battleshipoop/Game.java
@@ -6,7 +6,6 @@ public class Game {
     private MainWindow window;
 
     private int boardSize = 10;
-
     private int round = 0;
     private int player = 0;
 
@@ -15,6 +14,10 @@ public class Game {
 
     private Point2D[] player1Hits;
     private Point2D[] player2Hits;
+
+    public int getBoardSize() {
+        return boardSize;
+    }
 
     public Game(MainWindow window)
     {
