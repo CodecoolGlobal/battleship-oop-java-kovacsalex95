8279a368c87bc0334ead56b6cf6adff7da03b2ab
kovacsalex <kovacsalex95@gmail.com>
2021-05-26 17:20:57 +0200

    placement perfected, attack introduced

--- src/com/codecool/battleshipoop/Game.java
+++ src/com/codecool/battleshipoop/Game.java
@@ -1,6 +1,7 @@
 package com.codecool.battleshipoop;
 
 import java.awt.geom.Point2D;
+import java.util.Arrays;
 
 
 enum GameState {
@@ -88,37 +89,151 @@ public class Game {
 
         Point2D playerHighlight = fieldPanel.boardHighlight[player];
 
+        // TODO: hajók körül helyet kell hagyni
         if (fieldPanel.mouseInBoard() && playerHighlight != null) {
 
             // Hajó lerakás
             if (fieldPanel.mouseEvents.mouseLeftClick) {
 
+                int shipSize = SHIP_COUNT;
+                if (playerShips != null && playerShips[player] != null)
+                    shipSize = SHIP_COUNT - playerShips[player].length;
+
                 // Kezdőpont
-                if (shipPlacementPoint == null)
+                if (shipPlacementPoint == null) {
                     shipPlacementPoint = playerHighlight;
 
-                // Irány
-                else {
-                    // TODO: rendes ship generálás
-                    ShipPiece[] shipPieces = new ShipPiece[] {
-                        new ShipPiece((int)shipPlacementPoint.getX(), (int)shipPlacementPoint.getY(), false, ShipPart.SMALL)
-                    };
-
-                    Ship ship = new Ship(shipPieces, 0);
-                    playerShips[player] = Util.addShip(playerShips[player], ship);
-
-                    shipPlacementPoint = null;
+                    if (shipSize != 1)
+                        return;
                 }
+
+                // Small ship
+                if (shipSize == 1)
+                {
+                    Ship ship = new Ship(new ShipPiece[] { new ShipPiece(shipPlacementPoint, false, ShipPart.SMALL) }, 0);
+                    playerShips[player] = addShip(playerShips[player], ship);
+                    shipPlacementPoint = null;
+                    return;
+                }
+
+                // Large ship with angle
+                if (fieldPanel.boardHighlight == null || fieldPanel.boardHighlight[player] == null)
+                    return;
+
+                Point2D[] placementPoint = Util.getPlacementPoints(shipPlacementPoint, fieldPanel.boardHighlight[player], shipSize, boardSize);
+
+                if (placementPoint == null)
+                    return;
+
+                float placementAngle = Util.getPlacementAngle(shipPlacementPoint, fieldPanel.boardHighlight[player]);
+
+                ShipPiece[] shipPieces = new ShipPiece[placementPoint.length];
+                for (int i=0; i< shipPieces.length; i++)
+                {
+                    ShipPart part = ShipPart.MIDDLE;
+
+                    if (i == 0)
+                        part = ShipPart.REAR;
+                    else if (i == shipPieces.length - 1)
+                        part = ShipPart.FRONT;
+
+                    shipPieces[i] = new ShipPiece(placementPoint[i], false, part);
+                }
+
+                Ship ship = new Ship(shipPieces, placementAngle);
+                playerShips[player] = addShip(playerShips[player], ship);
+
+                shipPlacementPoint = null;
             }
 
             // Utolsó hajó törlése
             else if (fieldPanel.mouseEvents.mouseRightClick)
-                playerShips[player] = Util.removeShip(playerShips[player]);
+                playerShips[player] = removeShip(playerShips[player]);
         }
     }
 
 
     private void UpdateAttack() {
+        if (playerShipDestroyed(player))
+        {
+            // TODO: nyert XY
+            gameState = GameState.END;
+            return;
+        }
 
+        if (fieldPanel.boardHighlight == null || fieldPanel.boardHighlight[player == 0 ? 1 : 0] == null)
+            return;
+
+        Point2D enemyHighlight = fieldPanel.boardHighlight[player == 0 ? 1 : 0];
+
+        if (fieldPanel.mouseInBoard() && enemyHighlight != null && fieldPanel.mouseEvents.mouseLeftClick)
+        {
+            if (playerHits == null || playerHits[player] == null)
+                playerHits = new Point2D[2][0];
+
+            // ATTACK
+            for (int i=0; i<playerHits[player].length; i++)
+            {
+                if (playerHits[player][i].equals(enemyHighlight))
+                    return;
+            }
+
+            playerHits[player] = addHit(playerHits[player], enemyHighlight);
+
+            for (int i=0; i<playerShips[player == 0 ? 1 : 0].length; i++)
+            {
+                for (int j = 0; j < playerShips[player == 0 ? 1 : 0][i].shipPieces.length; j++)
+                {
+                    if (playerShips[player == 0 ? 1 : 0][i].shipPieces[j].position.equals(enemyHighlight))
+                        playerShips[player == 0 ? 1 : 0][i].shipPieces[j].hit = true;
+                }
+            }
+
+            player = player == 0 ? 1 : 0;
+        }
+    }
+
+
+    // MISC
+    public Ship[] addShip(Ship[] collection, Ship ship)
+    {
+        Ship[] result = new Ship[collection.length + 1];
+        for (int i=0; i < collection.length; i++)
+            result[i] = collection[i];
+        result[collection.length] = ship;
+        return result;
+    }
+
+    public Ship[] removeShip(Ship[] collection)
+    {
+        if (collection.length == 0) return collection;
+
+        Ship[] result = new Ship[collection.length - 1];
+        for (int i=0; i < result.length; i++)
+            result[i] = collection[i];
+        return result;
+    }
+
+    public Point2D[] addHit(Point2D[] collection, Point2D point)
+    {
+        Point2D[] result = new Point2D[collection.length + 1];
+        for (int i=0; i < collection.length; i++)
+            result[i] = collection[i];
+        result[collection.length] = point;
+        return result;
+    }
+
+    private boolean playerShipDestroyed(int player)
+    {
+        if (playerShips == null || playerShips[player] == null)
+            return false;
+
+        for (int i=0; i<playerShips[player].length; i++)
+        {
+            if (!playerShips[player][i].isDestroyed())
+                return false;
+        }
+
+        return true;
     }
 }
