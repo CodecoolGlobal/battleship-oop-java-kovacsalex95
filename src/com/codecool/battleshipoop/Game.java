3cc96648462f6f0721fb6733d4bf093142f28ec2
kovacsalex <kovacsalex95@gmail.com>
2021-05-26 11:29:29 +0200

    unnecessary comment removed

--- src/com/codecool/battleshipoop/Game.java
+++ src/com/codecool/battleshipoop/Game.java
@@ -5,6 +5,7 @@ import java.awt.geom.Point2D;
 public class Game {
     private MainWindow window;
 
+
     private int boardSize = 10;
     private int round = 0;
     private int player = 0;
@@ -18,11 +19,12 @@ public class Game {
     public int getBoardSize() {
         return boardSize;
     }
+    public void setBoardSize(int boardSize) {
+        this.boardSize = boardSize;
+    }
 
     public Game(MainWindow window)
     {
         this.window = window;
-
-        // player1Hits[0] = new Point2D.Double(0, 0);
     }
 }
