ccbb53240e7abe63d25e0dbc60a90e3696234e4e
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-05-26 16:11:04 +0200

    Create distance method and cell highlight

--- src/com/codecool/battleshipoop/Util.java
+++ src/com/codecool/battleshipoop/Util.java
@@ -2,6 +2,8 @@ package com.codecool.battleshipoop;
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 
 public class Util {
     public static Color fade(Color original, float alpha)
@@ -9,9 +11,14 @@ public class Util {
         return new Color((float)original.getRed() / 255f, (float)original.getGreen() / 255f, (float)original.getBlue() / 255f, alpha);
     }
 
+    public static Color rgbAColor(int r, int g, int b, float alpha)
+    {
+        return new Color((float)r / 255f, (float)g / 255f, (float)b / 255f, alpha);
+    }
+
     public static Color rgbColor(int r, int g, int b)
     {
-        return new Color((float)r / 255f, (float)g / 255f, (float)b / 255f);
+        return rgbAColor(r, g, b, 1);
     }
 
     public static AffineTransform rotateGraphics(Graphics2D g, float degrees, float x, float y)
@@ -40,4 +47,33 @@ public class Util {
             result[i] = collection[i];
         return result;
     }
+
+    public static void cellHighlight(Graphics2D g, Point2D cell, Color color, Rectangle2D rectangle, float cellSize) {
+        Rectangle2D highlightRectangle = new Rectangle2D.Double(rectangle.getX() + cell.getX() * cellSize, rectangle.getY() + cell.getY() * cellSize, cellSize, cellSize);
+
+        g.setPaint(color);
+        g.fill(highlightRectangle);
+    }
+
+    public static Point2D pointDistance(Point2D pointA, Point2D pointB) {
+        return new Point2D.Double(pointB.getX() - pointA.getX(), pointB.getY() - pointA.getY());
+    }
+
+    public static float normalize(float original) {
+        if (original == 0) return 0;
+        if (original < 0) return -1;
+        return 1;
+    }
+
+    public static Point2D pointDirection(Point2D pointA, Point2D pointB) {
+        Point2D distance = pointDistance(pointA, pointB);
+        if (pointA.equals(pointB) || normalize((float)Math.abs(distance.getX())) == normalize((float)Math.abs(distance.getY())))
+            return null;
+
+        float directionX = normalize((float)distance.getX());
+        float directionY = normalize((float)distance.getY());
+
+        return new Point2D.Double(directionX, directionY);
+    }
+
 }
