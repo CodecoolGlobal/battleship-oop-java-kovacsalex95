3203f98d37c92ba0a09c6e36ba1c0c5db4cee44f
KarpatiFeri <karpati.ferenc.93@gmail.com>
2021-05-27 10:43:31 +0200

    Draw credits function

--- src/com/codecool/battleshipoop/MainWindow.java
+++ src/com/codecool/battleshipoop/MainWindow.java
@@ -51,6 +51,7 @@ public class MainWindow {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // TODO: [BUTTON] Credits
+                window.fieldPanel.creditsMode = !window.fieldPanel.creditsMode;
             }
         });
 
